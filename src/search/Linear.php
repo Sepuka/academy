8a9659a4797c618151f03d40a881c99cfeff1005
Shlomin Maksim <m.shlomin@timeweb.ru>
2017-07-09 22:07:02 +0300

    first commit

---
+++ src/search/Linear.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace sepuka\academy\search;
+
+class Linear
+{
+    public function simpleLinearSymbolSearch(string $data, string $symbol): int
+    {
+        $len = mb_strlen($data);
+        $pos = 0;
+
+        while (($pos < $len) && ($data[$pos] != $symbol)) {
+            ++$pos;
+        }
+
+        return !isset($data[$pos]) ? -1 : $pos;
+    }
+
+    public function improvedLinearSymbolSearch(string $data, string $symbol): int
+    {
+        $len = mb_strlen($data);
+        $data .= $symbol;
+        $pos = 0;
+
+        while ($data[$pos] != $symbol) {
+            ++$pos;
+        }
+
+        return $pos == $len ? -1 : $pos;
+    }
+}
---
+++ test/search/LinearTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace sepuka\academy\test\search;
+
+use PHPUnit\Framework\TestCase;
+use sepuka\academy\search\Linear;
+
+class LinearTest extends TestCase
+{
+    /** @var Linear */
+    private $linearInstance;
+
+    public function setUp()
+    {
+        $this->linearInstance = new Linear();
+    }
+
+    /**
+     * @dataProvider simpleLinearDataProvider
+     * @param string $data
+     * @param string $symbol
+     * @param int $expectedPos
+     */
+    public function testSimpleLinear(string $data, string $symbol, int $expectedPos)
+    {
+        $this->assertEquals($expectedPos, $this->linearInstance->simpleLinearSymbolSearch($data, $symbol));
+    }
+
+    /**
+     * @dataProvider simpleLinearDataProvider
+     * @param string $data
+     * @param string $symbol
+     * @param int $expectedPos
+     */
+    public function testImprovedLinear(string $data, string $symbol, int $expectedPos)
+    {
+        $this->assertEquals($expectedPos, $this->linearInstance->improvedLinearSymbolSearch($data, $symbol));
+    }
+
+    /**
+     * @return array
+     */
+    public function simpleLinearDataProvider(): array
+    {
+        return [
+            ['qwerty', 'q', 0],
+            ['qwerty', 'w', 1],
+            ['qwerty', 'y', 5],
+            ['qwerty', 'a', -1],
+        ];
+    }
+}
