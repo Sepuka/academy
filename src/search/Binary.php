4cf309d8effe447d18eaa810dedeac189a7d833e
Maksim Shlomin <maksim.shlomin@iqoption.com>
2017-07-10 18:43:16 +0300

    binary search

---
+++ src/search/Binary.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace sepuka\academy\search;
+
+class Binary
+{
+    /**
+     * @param string $data
+     * @param int    $value
+     *
+     * @return int
+     */
+    public function simpleSearch(string $data, int $value): int
+    {
+        $l = 0;
+        $r = strlen($data);
+        $middle = intdiv($r, 2);
+
+        while (($l <= $r) && $data[$middle] != $value) {
+            if ($data[$middle] < $value) {
+                $l = $middle + 1;
+                $middle = (int)floor(($l + $r)/2);
+            } else {
+                $r = $middle - 1;
+                $middle = (int)ceil(($l + $r)/2);
+            }
+        }
+
+        return $middle;
+    }
+
+    /**
+     * @param string $data
+     * @param int    $value
+     *
+     * @return int
+     */
+    public function improvedSearch(string $data, int $value): int
+    {
+        $l = 0;
+        $r = strlen($data);
+
+        while ($l < $r) {
+            $middle = (int)floor(($l + $r) / 2);
+            if ($data[$middle] < $value) {
+                $l = $middle + 1;
+            } else {
+                $r = $middle;
+            }
+        }
+
+        return $r;
+    }
+}
---
+++ test/search/BinaryTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace sepuka\academy\test\search;
+
+use PHPUnit\Framework\TestCase;
+use sepuka\academy\search\Binary;
+
+class BinaryTest extends TestCase
+{
+    /** @var Binary */
+    private $instance;
+
+    public function setUp()
+    {
+        $this->instance = new Binary();
+    }
+
+    /**
+     * @dataProvider simpleSearchDataProvider
+     * @param string $data
+     * @param int    $value
+     * @param int    $expectedIndex
+     */
+    public function testSimpleSearch(string $data, int $value, int $expectedIndex)
+    {
+        $this->assertEquals($expectedIndex, $this->instance->simpleSearch($data, $value));
+    }
+
+    /**
+     * @dataProvider simpleSearchDataProvider
+     * @param string $data
+     * @param int    $value
+     * @param int    $expectedIndex
+     */
+    public function testImprovedSearch(string $data, int $value, int $expectedIndex)
+    {
+        $this->assertEquals($expectedIndex, $this->instance->improvedSearch($data, $value));
+    }
+
+    public function simpleSearchDataProvider(): array
+    {
+        return [
+            ['0123456789', 0, 0],
+            ['0123456789', 1, 1],
+            ['0123456789', 2, 2],
+            ['0123456789', 3, 3],
+            ['0123456789', 4, 4],
+            ['0123456789', 5, 5],
+            ['0123456789', 6, 6],
+            ['0123456789', 7, 7],
+            ['0123456789', 8, 8],
+            ['0123456789', 9, 9],
+        ];
+    }
+}
