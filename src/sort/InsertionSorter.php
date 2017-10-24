1b5e31b902cf0d445d8b69888c260cab968bacdb
Shlomin Maksim <m.shlomin@timeweb.ru>
2017-10-24 22:25:37 +0300

    Простой алгоритм сортировки вставками

---
+++ src/sort/InsertionSorter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace sepuka\academy\sort;
+
+/**
+ * Реализации алгоритмов сортировки
+ * 1. Простая сортировка вставками, устойчивый алгоритм
+ * 2. Сортировка двоичными вставками
+ */
+class InsertionSorter
+{
+    /**
+     * @param array $items
+     * @TODO: тесты
+     *
+     * @return array
+     */
+    public function straightInsertionSorter(array $items): array
+    {
+        for($i = 0; $i < count($items); ++$i) {
+            $current = $items[$i];
+            $j = $i;
+            while ($j > 0 && $current < $items[$j - 1]) {
+                $items[$j] = $items[$j - 1];
+                --$j;
+            }
+            $items[$j] = $current;
+        }
+
+        return $items;
+    }
+}
